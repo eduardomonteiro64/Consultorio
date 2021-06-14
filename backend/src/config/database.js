66a7208b3d7697cd9eb110e7b219c8fe0e489301
Marcos Manoel <marcosmss1304@gmail.com>
2021-06-14 14:38:00 -0300

    config database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.set("useFindAndModify", false);
 module.exports = mongoose
-  .connect("mongodb://localhost/consultorio", {
+  .connect("mongodb+srv://admin2:admin@cluster0.apili.mongodb.net/db_consultorio?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then((conn) => console.warn("ok"))
-  .catch((err) => console.warn("erro"));
+  .catch((err) => console.warn("erro: ", ));
