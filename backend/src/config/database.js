feda49ce631f9b5c95a37e9f38ada3bc175de8d7
eduardo.carm <eduardo.carmo@sbwebservicesext.net>
2021-06-15 01:27:23 -0300

    equalize app

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.set("useFindAndModify", false);
 module.exports = mongoose
-  .connect("mongodb+srv://admin2:admin@cluster0.apili.mongodb.net/db_consultorio?retryWrites=true&w=majority", {
+  .connect("mongodb://localhost/consultorio", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then((conn) => console.warn("ok"))
-  .catch((err) => console.warn("erro: ", ));
+  .catch((err) => console.warn("erro: "));
